b882adba1d7a290131deb714e9c24b04815c4f6e
Hai Nguyen Thanh <nthanhhai2909@gmail.com>
2017-11-30 23:26:50 +0700

    tao suon cho project

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var path = require('path');
+var cors = require('cors')
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/server_blog_db');
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
+});
+app.listen(port, ()=> console.log("Server running on port " + port));
