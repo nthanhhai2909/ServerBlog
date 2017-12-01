b5baddcc1562a1c6fd339003c42795ab01e339e0
Hai Nguyen Thanh <nthanhhai2909@gmail.com>
2017-12-01 23:47:09 +0700

    them chuc nang kiem soat tinh hinh dang nhap bang jwt

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,44 +1,95 @@
+
 'use strict'
 var mongoose = require('mongoose');
 var User = mongoose.model('user');
 var passwordHash = require('password-hash');
+var jwt = require('jsonwebtoken');
 
 exports.login = (req, res) => {
-    User.findOne({'username': req.body.username}, 'username password _id', (err, data)=>{
+    // check data client send invalid
+    if((typeof req.body.username == 'undefined') 
+        || (typeof req.body.password == 'undefined')){
+
+        res.json({status: 'false'});
+        return;
+    }
+
+    // verify account
+     User.findOne({'username': req.body.username}, 'username password _id', (err, data)=>{
         //Find error
         if(err){
-            res.send(err);
+            res.json(err);
             return;
         }
         // User not exit
-        if(!data){
-            res.send({status: 'false'});
+        if(typeof data == 'undefined'){
+            res.json({status: 'false'});
             return;
         }
         // Password invalid
         let hashedPassword = data.password;
+
         if(!passwordHash.verify(req.body.password, hashedPassword)){
-            res.send({status: 'false'});
+            res.json({status: 'false'});
+            return;
         }
-        res.json({status: 'true', id:data._id});   
+
+        // award token for account login & check is login befor
+        if(typeof req.body.token == 'undefined'){
+            let token = jwt.sign({username: req.body.username, iat: Math.floor(Date.now() / 1000)}, 'shhhhh');
+            res.json({status: 'true', id:data._id, token: token}); 
+            return;
+        }
+        // verify a token symmetric
+        jwt.verify(req.body.token, 'shhhhh', (err, decore) => {
+            if(err){
+                res.json({status:'false'});
+                return;
+            }
+            if(Math.floor((Date.now() / 1000)) <=  (decore.iat + 100* 20)){
+                console.log('hihi');
+                let token = jwt.sign({ username: req.body.username, iat:  Math.floor(Date.now() / 1000) }, 'shhhhh');
+                res.json({status: 'true', id:data._id, token: token}); 
+                return;
+            }
+            else{
+                res.json({status: 'false'});
+            }
+
+        });
+        
     });
+
+    
+      
+
+    
 };  
 
 exports.signup = (req, res) => {
+
+    if((typeof req.body.username == 'undefined')
+         || (typeof req.body.password == 'undefined')
+            || typeof req.body.confirm == 'undefined'
+            || typeof req.body.email == 'undefined'){
+        res.json({status: 'false'});
+        return;
+    }
+
     let hashedPassword = passwordHash.generate(req.body.password);
     User.find({$or:[{username:req.body.username}, {email:req.body.email}]}, (err, user) => {
         if(err){
-            res.send(err);
+            res.json(err);
             return;
         }
         // User Already
-        if(user){
+        if(user.length > 0){
             res.send({status: 'false', message: "User Already exsit"});
             return;
         }
         // Password and confirm 
         if(req.body.password != req.body.confirm){
-            res.send({status: 'false', message:""});
+            res.json({status: 'false', message:""});
             return;
         }
         var new_user = new User({
@@ -49,10 +100,10 @@ exports.signup = (req, res) => {
         });
         new_user.save((err, data) =>{
             if(err){
-                res.send({status: 'false', message:""});
+                res.json({status: 'false', message:""});
             }
             else{
-                res.send({status: 'true', message:""});
+                res.json({status: 'true', message:""});
             }
         });   
 
